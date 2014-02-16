f1bbc6af7cd0b8d019a0bde2fcee5a8c35da569f
xhj1102 <haoji.xu@me.com>
2014-02-16 09:41:30 +0800

    Update main.c

--- main.c
+++ main.c
@@ -1,6 +1,11 @@
 //怎么了有问题？
 #incluce <stdio.h>
+char s[1000];
 int main()
 {
+    printf("请输入待计算的字符串。帮助函数为help()，返回值为0。")
+    scanf("%s",&s);
+    //我真的觉得IDE比在这里写代码好
+    split(s);
     return 0;
 }
