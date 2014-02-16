da8010d0d12db8d6edb433e7ddd7bc581569a173
xhj1102 <haoji.xu@me.com>
2014-02-16 10:05:46 +0800

    Update main.c

--- main.c
+++ main.c
@@ -1,8 +1,12 @@
 #incluce <stdio.h>
 char s[1000];
+void split(char c[])
+{
+
+}
 int main()
 {
-    printf("请输入待计算的字符串。帮助函数为help()，返回值为0。")
+    printf("请输入待计算的字符串。帮助函数为help()，返回值为0。");
     scanf("%s",&s);
     split(s);
     return 0;
